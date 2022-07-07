384117f0368b41857f6e963b2e117b7e6d33485a
Serje <Serje.havandjian@gmail.com>
2022-07-07 16:05:10 -0700

    commit tree 292cb3665b09f8036a1ba593aec4db674839b6b7

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,46 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    checkP = Stack.new
+
+
+    string.chars.each do |char|
+
+        if checkP.peek == "(" && char == ")"
+            checkP.pop
+        else
+            checkP.push(char)
+        end
+
+    end
+   
+    puts checkP.size
+
+
+
+ 
+end
+
+balancing_parentheses("())()")
+
+
+
+# checkP = string
+
+# first = checkP.length[-1]
+# last = checkP.length
+
+# puts first
+# puts last
+
+
+
+# if first == "("
+#     puts "test"
+# end
+
+
+
+# puts checkP
+# puts checkP[0]
+# puts test
